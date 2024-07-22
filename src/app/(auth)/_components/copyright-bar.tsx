c57ee62e3003f6cf0ee6b9a5502bc0eba276e3bb
Afief Abdurrahman <91366510+mafiefa02@users.noreply.github.com>
2024-07-22 09:42:43 +0800

    chore: responsiveness fix on copyright bar

--- src/app/(auth)/_components/copyright-bar.tsx
+++ src/app/(auth)/_components/copyright-bar.tsx
@@ -27,7 +27,7 @@ export const CopyrightBar = () => {
   ];
 
   return (
-    <footer className="dark:bg-dark-body bg-light-body sticky bottom-0 w-full py-1 text-[0.6rem]">
+    <footer className="sticky bottom-0 w-full bg-light-body py-1 text-[0.6rem] dark:bg-dark-body">
       <div className="container flex items-center justify-between gap-4">
         <div className="group flex items-center gap-[3px]">
           <span className="font-semibold">&copy; 2024</span>
@@ -36,7 +36,10 @@ export const CopyrightBar = () => {
             href="https://mbss.sch.id"
             target="_blank"
           >
-            Madina Boarding School Samarinda
+            <span className="hidden sm:inline-block">
+              Madina Boarding School Samarinda
+            </span>
+            <span className="sm:hidden">MBS Samarinda</span>
           </Link>
         </div>
 
