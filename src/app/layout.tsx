c61460c6a8d83ab5242e50f333de3a309f8bde29
Afief Abdurrahman <91366510+mafiefa02@users.noreply.github.com>
2024-07-22 15:16:49 +0800

    chore: trying out changing mantine's and tailwind's styles import order

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,4 @@
 import '@/styles/globals.css';
-import '@mantine/core/styles.css';
 import { ColorSchemeScript, MantineProvider } from '@mantine/core';
 import type { Metadata } from 'next';
 import { Toaster } from 'sonner';
