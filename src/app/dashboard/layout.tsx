20e6aef3a38fe48c5881f7b9200e712847162212
Afief Abdurrahman <91366510+mafiefa02@users.noreply.github.com>
2024-07-24 18:02:12 +0800

    chore: dashboard layout changes

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -13,10 +13,10 @@ const DashboardLayout = ({
   return (
     <div className="grid h-dvh grid-rows-[auto_1fr]">
       <NavigationBar />
-      <div className="container grid grid-rows-[auto_1fr] gap-4 overflow-y-hidden py-4">
-        <DashboardHeader />
-        <div className="relative grid grid-cols-[auto_1fr] gap-5 overflow-y-auto">
-          <DashboardSidebar />
+      <div className="container grid grid-cols-[auto_1fr] gap-4 overflow-y-hidden py-4">
+        <DashboardSidebar />
+        <div className="relative grid grid-rows-[auto_1fr] gap-5 overflow-y-auto">
+          <DashboardHeader />
           <div className="no-scrollbar overflow-y-auto">{children}</div>
         </div>
       </div>
