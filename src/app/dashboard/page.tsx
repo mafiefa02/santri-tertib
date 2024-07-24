f158c544fba053e9085f9bef0e8d0ea54951ac4f
Afief Abdurrahman <91366510+mafiefa02@users.noreply.github.com>
2024-07-24 20:42:41 +0800

    fix: add header in root dashboard page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -11,55 +11,64 @@ import { Suspense } from 'react';
 import { TableBodyLoading } from '@/components/table-body-loading';
 import prisma from '@/config/db';
 
+import { DashboardHeader } from './_components/dashboard-header';
 import { LastLogin } from './_components/last-login';
 import {
   StatisticsCard,
   StatisticsCardLoading,
 } from './_components/statistics-card';
 
-const columns = ['Account Name', 'Username', 'Type', 'Login Time'];
-
 const DashboardPage = () => {
   return (
-    <div className="flex flex-col gap-5">
-      <div className="grid grid-cols-1 gap-x-3 gap-y-2 mtn-sm:grid-cols-3">
-        <Suspense fallback={<StatisticsCardLoading />}>
-          <StatisticsCard
-            fetchFn={prisma.rewardRecord.count()}
-            name="Rewards"
-          />
-        </Suspense>
-        <Suspense fallback={<StatisticsCardLoading />}>
-          <StatisticsCard
-            fetchFn={prisma.violationRecord.count()}
-            name="Violations"
-          />
-        </Suspense>
-        <Suspense fallback={<StatisticsCardLoading />}>
-          <StatisticsCard fetchFn={prisma.leaveRecord.count()} name="Permits" />
-        </Suspense>
-      </div>
-
-      <TableScrollContainer minWidth={768}>
-        <Paper withBorder>
-          <Table highlightOnHover stickyHeader>
-            <TableThead>
-              <TableTr className="text-mtn-primary-filled dark:text-mtn-primary-light-color">
-                {columns.map((column) => (
-                  <TableTh key={`${column}column`}>{column}</TableTh>
-                ))}
-              </TableTr>
-            </TableThead>
-            <Suspense
-              fallback={<TableBodyLoading columnCount={columns.length} />}
-            >
-              <LastLogin />
+    <div className="relative grid grid-rows-[auto_1fr] gap-5 overflow-y-auto">
+      <DashboardHeader />
+      <div className="no-scrollbar overflow-y-auto">
+        <div className="flex flex-col gap-5">
+          <div className="grid grid-cols-1 gap-x-3 gap-y-2 mtn-sm:grid-cols-3">
+            <Suspense fallback={<StatisticsCardLoading />}>
+              <StatisticsCard
+                fetchFn={prisma.rewardRecord.count()}
+                name="Rewards"
+              />
             </Suspense>
-          </Table>
-        </Paper>
-      </TableScrollContainer>
+            <Suspense fallback={<StatisticsCardLoading />}>
+              <StatisticsCard
+                fetchFn={prisma.violationRecord.count()}
+                name="Violations"
+              />
+            </Suspense>
+            <Suspense fallback={<StatisticsCardLoading />}>
+              <StatisticsCard
+                fetchFn={prisma.leaveRecord.count()}
+                name="Permits"
+              />
+            </Suspense>
+          </div>
+
+          <TableScrollContainer minWidth={768}>
+            <Paper withBorder>
+              <Table highlightOnHover stickyHeader>
+                <TableThead>
+                  <TableTr className="text-mtn-primary-filled dark:text-mtn-primary-light-color">
+                    {columns.map((column) => (
+                      <TableTh key={`${column}column`}>{column}</TableTh>
+                    ))}
+                  </TableTr>
+                </TableThead>
+                <Suspense
+                  fallback={<TableBodyLoading columnCount={columns.length} />}
+                >
+                  <LastLogin />
+                </Suspense>
+              </Table>
+            </Paper>
+          </TableScrollContainer>
+        </div>
+      </div>
     </div>
   );
 };
+
+const columns = ['Account Name', 'Username', 'Type', 'Login Time'];
 
 export default DashboardPage;
