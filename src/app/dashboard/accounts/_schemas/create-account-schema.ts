c55ea12a5bd2f44509f84aabac2ce3d9f0c65d86
Afief Abdurrahman <91366510+mafiefa02@users.noreply.github.com>
2024-07-30 10:49:23 +0800

    feat: extra no space validation in username field

--- src/app/dashboard/accounts/_schemas/create-account-schema.ts
+++ src/app/dashboard/accounts/_schemas/create-account-schema.ts
@@ -3,7 +3,13 @@ import { z } from 'zod';
 
 export const createAccountSchema = z.object({
   displayName: z.string().min(1, { message: 'Display name is required!' }),
-  username: z.string().min(1, { message: 'Username is required!' }),
+  username: z
+    .string()
+    .min(8, { message: 'Username needs to be atleast 8 characters long!' })
+    .transform((arg) => arg.trim())
+    .refine((arg) => !arg.split('').includes(' '), {
+      message: "Username can't contain spaces between words.",
+    }),
   password: z
     .string()
     .min(8, { message: 'Password needs to be atleast 8 characters long!' }),
