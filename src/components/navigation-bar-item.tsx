7a7471cc91577fbd884b47eec9ea367e4ba8e5eb
Afief Abdurrahman <91366510+mafiefa02@users.noreply.github.com>
2024-08-03 20:38:16 +0800

    refactor: only render one button element for navigation bar item

--- src/components/navigation-bar-item.tsx
+++ src/components/navigation-bar-item.tsx
@@ -1,10 +1,13 @@
 'use client';
 
 import { Button } from '@mantine/core';
+import { useMediaQuery } from '@mantine/hooks';
 import type { $Enums } from '@prisma/client';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import React from 'react';
+
+import { cn } from '@/utils/cn';
 
 interface MenuItem {
   href: string;
@@ -16,53 +19,26 @@ interface MenuItem {
 export const NavigationBarItem = ({ menu }: { menu: MenuItem }) => {
   const pathname = usePathname();
   const isActive = pathname.startsWith(menu.href);
-
-  if (isActive)
-    return (
-      <Button
-        key={menu.href}
-        aria-label={`Navigation ${menu.label}`}
-        className="shrink-0"
-        component={Link}
-        href={menu.href}
-        justify="center"
-        leftSection={menu.icon}
-        size="xs"
-        variant="light"
-      >
-        {menu.label}
-      </Button>
-    );
+  const matches = useMediaQuery('(min-width: 36em)'); // matches with mtn-xs
+  const shouldRenderLabel = isActive || matches;
 
   return (
-    <>
-      <Button
-        key={`${menu.href}mobile`}
-        aria-label={`Navigation ${menu.label}`}
-        className="shrink-0"
-        classNames={{ root: 'mtn-xs:hidden' }}
-        component={Link}
-        href={menu.href}
-        justify="center"
-        size="compact-sm"
-        variant="subtle"
-      >
-        {menu.icon}
-      </Button>
-      <Button
-        key={`${menu.href}desktop`}
-        aria-label={`Navigation ${menu.label}`}
-        className="shrink-0"
-        classNames={{ root: 'hidden mtn-xs:block' }}
-        component={Link}
-        href={menu.href}
-        justify="center"
-        leftSection={menu.icon}
-        size="xs"
-        variant="subtle"
-      >
-        {menu.label}
-      </Button>
-    </>
+    <Button
+      key={menu.href}
+      aria-label={`Navigate to ${menu.label}`}
+      component={Link}
+      href={menu.href}
+      justify="center"
+      leftSection={shouldRenderLabel ? menu.icon : undefined}
+      size={shouldRenderLabel ? 'xs' : 'compact-sm'}
+      variant={isActive ? 'light' : 'subtle'}
+      className={cn(
+        'shrink-0',
+        !isActive && matches ? 'hidden mtn-xs:block' : null,
+        !isActive && !matches ? 'mtn-xs:hidden' : null,
+      )}
+    >
+      {shouldRenderLabel ? menu.label : menu.icon}
+    </Button>
   );
 };
