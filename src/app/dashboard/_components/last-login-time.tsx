83e886a8de330b1ef33a9b2a0191b24ad421db95
Afief Abdurrahman <91366510+mafiefa02@users.noreply.github.com>
2024-08-09 17:16:44 +0800

    fix: hydration error on login time

--- src/app/dashboard/_components/last-login-time.tsx
+++ src/app/dashboard/_components/last-login-time.tsx
@@ -1,10 +1,14 @@
 'use client';
 
+import { Skeleton } from '@mantine/core';
+import { useMounted } from '@mantine/hooks';
 import dayjs, { extend } from 'dayjs';
 import LocalizedFormat from 'dayjs/plugin/localizedFormat';
 
 extend(LocalizedFormat);
 
 export const LastLoginTime = ({ loginTime }: { loginTime: Date }) => {
-  return dayjs(loginTime).format('LL HH:mm:ss');
+  const mounted = useMounted();
+  if (!mounted) return <Skeleton className="h-5 w-full" />;
+  return dayjs(loginTime).format('LL LTS');
 };
