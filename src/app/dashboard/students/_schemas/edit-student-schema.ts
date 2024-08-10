4de264b9528328a6fa7b087cfca7137c80ac3b44
Afief Abdurrahman <mafiefa02@gmail.com>
2024-08-11 00:18:12 +0800

    fix: remove min length in student address schema

--- src/app/dashboard/students/_schemas/edit-student-schema.ts
+++ src/app/dashboard/students/_schemas/edit-student-schema.ts
@@ -7,10 +7,7 @@ export const editStudentSchema = z.object({
   identityNumber: z
     .string({ required_error: "Please provide student's identifier!" })
     .min(1, { message: "Please provide student's identifier!" }),
-  address: z
-    .string()
-    .min(5, { message: 'Address needs to be atleast 5 characters long!' })
-    .optional(),
+  address: z.string().optional(),
   classId: z.string().refine((value) => {
     const id = parseInt(value);
     return !isNaN(parseInt(value)) && id >= 0;
